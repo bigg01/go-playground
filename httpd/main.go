1b1013fcdb84b4bf5654c4cae1d20732edf49465
Oliver Guggenbuehl <o.guggenbuehl@gmail.com>
2017-01-01 11:57:38 +0100

    get hostname

--- httpd/main.go
+++ httpd/main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"strings"
 	"log"
-	"time"
+	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()  // parse arguments, you have to call this by yourself
-	fmt.Println(r.Form)  // print form information in server side
+	r.ParseForm()       // parse arguments, you have to call this by yourself
+	fmt.Println(r.Form) // print form information in server side
 	fmt.Println("path", r.URL.Path)
 	fmt.Println("scheme", r.URL.Scheme)
 	fmt.Println(r.Form["url_long"])
@@ -23,23 +23,30 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	now := time.Now()
 
 	host, err := os.Hostname()
+
 	if err != nil {
 		fmt.Println("cannot get hostname panic !!!!")
-		panic(err)
+		//panic(err)
+	} else {
+		fmt.Println(host)
 	}
 
-
 	fmt.Println(now)
-	fmt.Println(host)
+	if host != "" {
+		host = os.Getenv("HOSTNAME")
+		fmt.Println(host)
+
+	}
+
 	var name string
 	name = "oliver go"
 	name2 := "I love Vanessa"
-	fmt.Fprintf(w, "Hello " + name + " " + name2 + " " + now.String() + ": " + host) // send data to client side
+	fmt.Fprintf(w, "Hello "+name+" "+name2+" "+now.String()+": "+host) // send data to client side
 	//fmt.Fprintf(w, "Hello " + name + " " + name2 ) // send data to client side
 }
 
 func main() {
-	http.HandleFunc("/", sayhelloName) // set router
+	http.HandleFunc("/", sayhelloName)       // set router
 	err := http.ListenAndServe(":9090", nil) // set listen port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
