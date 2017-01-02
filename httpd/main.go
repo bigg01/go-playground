f8db801bf64dfee231b8de13c2f29cf144d17109
Oliver Guggenbuehl <o.guggenbuehl@gmail.com>
2017-01-02 11:24:43 +0100

    get hostname

--- httpd/main.go
+++ httpd/main.go
@@ -2,14 +2,37 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strings"
 	"time"
-
 	//log "github.com/cihub/seelog"
 )
+
+func check(e error) bool {
+	if e != nil {
+		log.Fatal(e)
+		return false
+	} else {
+		return true
+	}
+
+}
+
+func test(filename string) {
+	d1 := []byte("hello\ngo\n")
+	log.Printf("writeing to file %v", filename)
+	err := ioutil.WriteFile(filename, d1, 0644)
+	check(err)
+}
+
+func test2(filename string) {
+	f, err := os.Create(filename)
+	check(err)
+	defer f.Close()
+}
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()       // parse arguments, you have to call this by yourself
@@ -25,37 +48,44 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	now := time.Now()
 
 	/*
-	host, err := os.Hostname()
-	if err != nil {
-		fmt.Println("cannot get hostname panic !!!!")
-		//panic(err)
-	} else {
-		fmt.Println(host)
-	}
+		host, err := os.Hostname()
+		if err != nil {
+			fmt.Println("cannot get hostname panic !!!!")
+			//panic(err)
+		} else {
+			fmt.Println(host)
+		}
 	*/
 	host := os.Getenv("HOSTNAME")
 
 	/*
-	if host != "" {
+		if host != "" {
 
-		host = "na"
+			host = "na"
 
-	}
+		}
 	*/
 
 	//var name string
 	//name = "oliver go"
 	//name2 := "I love Vanessa"
-	fmt.Fprintf(w, "Hello Go test - " + now.String() + ": " +host) // send data to client side
-	log.Println("Hello Go test - " + now.String() + ": " +host)
+	fmt.Fprintf(w, "Hello Go test - "+now.String()+": "+host) // send data to client side
+	log.Println("Hello Go test - " + now.String() + ": " + host)
 	//fmt.Fprintf(w, "Hello " + name + " " + name2 ) // send data to client side
+
 }
 
 func main() {
+	log.Println("write file")
+
+	test("filename.txt")
+	test2("filename2.txt")
+
 	log.Println("Starting Server !")
 	http.HandleFunc("/", sayhelloName)       // set router
 	err := http.ListenAndServe(":9090", nil) // set listen port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+
 }
